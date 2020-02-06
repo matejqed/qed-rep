175f14db3eec879b68ab9b0c53dbc5c013a7410c
matejqed <60287737+matejqed@users.noreply.github.com>
2020-02-06 13:50:20 +0100

    finished form

---
+++ JS/bootstrap-validate.js
@@ -0,0 +1,26 @@
+/*
+const Fname = document.getElementById('Fname')
+const Lname = document.getElementById('Lname')
+const Email = document.getElementById('Email')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('submit'), (e) => {
+    let messages = []
+    if (Fname.value === '' || Fname.value == null) {
+        messages.push('Invalid input, please enter your First Name.')
+    }
+
+    if (Lname.value === '' || Lname.value == null) {
+        messages.push('Invalid input, please enter your Last Name.')
+    }
+
+    if (Email.value === '' || Lname.value == null) {
+        messages.push('Invalid input, please enter a valid Email adress.')
+    }
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join('')
+    }
+}
+*/
