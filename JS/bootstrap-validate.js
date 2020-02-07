cba6522e74ad0205251193fe9d1dc92ba1ca4183
matejqed <60287737+matejqed@users.noreply.github.com>
2020-02-07 10:41:16 +0100

    Fixed JS on Form

--- JS/bootstrap-validate.js
+++ JS/bootstrap-validate.js
@@ -1,26 +1,24 @@
-/*
 const Fname = document.getElementById('Fname')
 const Lname = document.getElementById('Lname')
 const Email = document.getElementById('Email')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 
-form.addEventListener('submit'), (e) => {
-    let messages = []
-    if (Fname.value === '' || Fname.value == null) {
-        messages.push('Invalid input, please enter your First Name.')
-    }
+form.addEventListener('submit', (e) => {
+            let messages = []
+            if (Fname.value === '' || Fname.value == null) {
+                messages.push('Invalid input, please enter your First Name.')
+            }
 
-    if (Lname.value === '' || Lname.value == null) {
-        messages.push('Invalid input, please enter your Last Name.')
-    }
+            if (Lname.value === '' || Lname.value == null) {
+                messages.push('Invalid input, please enter your Last Name.')
+            }
 
-    if (Email.value === '' || Lname.value == null) {
-        messages.push('Invalid input, please enter a valid Email adress.')
-    }
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join('')
-    }
-}
-*/
+            if (Email.value === '' || Lname.value == null) {
+                messages.push('Invalid input, please enter a valid Email adress.')
+            }
+            if (messages.length > 0) {
+                e.preventDefault()
+                errorElement.innerText = messages.join('')
+            }
+        )
